b61be8637870dad3be8fc80c2d3a415029728999
schplorf <65134690+LJ3D@users.noreply.github.com>
2022-12-18 14:47:55 +0000

    Basic echoing going on :+1:

--- echo-server/echo-server.cpp
+++ echo-server/echo-server.cpp
@@ -3,9 +3,17 @@
 
 #include "torplusplus.hpp"
 
-#define HOST "..."
+#include "gitIgnoredOnionAddress.h" // HOST is defined in here, but it's not in the repo because i want to hide my onion address ;)
+// Remove the above line and replace it with the following line with your own onion address:
+// #define HOST ".........................................................onion"
 
 int main(){
-    // ....
+    torPlusPlus::torSocket torSock;
+    torSock.startProxy(); // Use default settings
+    torSock.connectTo(HOST); // Connect the proxy to the onion address (Default port is 80)
+    torSock.proxySend("Hello World!", 12); // Send a message to the hidden service
+    char buf[16384] = {0}; // Up to 16KB of memory for whatever gets sent back
+    torSock.proxyRecv(buf, sizeof(buf) * sizeof(char)); // Receive a response
+    printf("%s\n", buf); // Print whatever the server sent back
     return 0;
 }
