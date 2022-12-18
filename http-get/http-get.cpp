7d9db50abefcaec108b6a9fb6cadaac741e2cb91
schplorf <65134690+LJ3D@users.noreply.github.com>
2022-12-18 11:46:01 +0000

    testing branch

--- http-get/http-get.cpp
+++ http-get/http-get.cpp
@@ -3,13 +3,16 @@
 
 #include "torplusplus.hpp"
 
-#define TEST_ONION "cryptbbtg65gibadeeo2awe3j7s6evg7eklserehqr4w4e2bis5tebid.onion"
+#define HOST "cryptbbtg65gibadeeo2awe3j7s6evg7eklserehqr4w4e2bis5tebid.onion"
 
 int main(){
-    torSocketGlobals::DEBUG = true; // Enable debug messages
-    torSocket torSock; // Create a torSocket object. Doing this starts the TOR proxy and connects to it
-    torSock.connectTo(TEST_ONION); // Connect the proxy to the onion address
-    std::string httpReq = "GET / HTTP/1.1\r\nHost: " + std::string(TEST_ONION) + "\r\n\r\n"; // Assemble a HTTP GET request to send to the site
+    torPlusPlus::torSocket torSock; // Create a torSocket object.
+    torSock.startProxy(".\\tor\\tor.exe", // Path to the tor executable
+                       10, // Number of seconds to wait for tor to start 
+                       "127.0.0.1", // Address of the proxy
+                       9050); // Port to connect to the proxy on
+    torSock.connectTo(HOST); // Connect the proxy to the onion address
+    std::string httpReq = "GET / HTTP/1.1\r\nHost: " + std::string(HOST) + "\r\n\r\n"; // Assemble a HTTP GET request to send to the site
     torSock.proxySend(httpReq.c_str(), (int)httpReq.length()); // Send the request to the hidden service
     char buf[16384] = {0}; // Up to 16KB of memory for whatever gets sent back
     torSock.proxyRecv(buf, sizeof(buf) * sizeof(char)); // Receive a response to the GET request
